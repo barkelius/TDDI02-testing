75b262351a8e156a8fa9626bd88df55d99c27fab
Axeba415 <barkelius@gmail.com>
2013-11-15 13:39:42 +0100

    arvid kollar
    asdasdas

--- playertest.cc
+++ playertest.cc
@@ -10,6 +10,35 @@ int main(){
     friend1.printAllied();
     Allied friend2("adventure.txt",1003);
     friend2.printAllied();
-    //testar
+    
+   
+    //<----otestat start------> // bugg, läser in sluttecknet '¤' om itemet är 
+    //det sista i rummet.... dvs 2008;asd;asda;¤ kommer ligga först i filen
+    vector <string> lines;
+    ifstream inFile;
+    string str,str2;
+    int itemId = 2008;
+    int lineToMove = 0;    
+    inFile.open("adventure2.txt");
+    while(!inFile.eof()){
+      getline(inFile,str,';');
+      if(stringToInt(str) == itemId)
+	lineToMove = lines.size();
+      getline(inFile,str2);
+      lines.push_back(str + ";" + str2);
+    }
+    inFile.close();
+    string removeFile = "adventure3.txt";
+    if(remove(removeFile.c_str()) != 0 )
+      cerr << "failed to remove file" << removeFile << endl;
+    ofstream outFile;
+    lines.insert(lines.begin(),lines[lineToMove]);
+    lines.erase(lines.begin()+lineToMove+1);
+    outFile.open("adventure3.txt");
+    for(int i = 0; i < lines.size(); ++i){  
+      outFile << lines[i] << "\n";
+    }
+    //<----otestat end------>
+
 return 0;
 }
