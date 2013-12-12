17bb50b042fb5c07275f2df18623366e5fab115c
arvjo <riavd18@gmail.com>
2013-12-12 14:29:40 +0100

    Menu.cpp med kommentarer

--- Menu.cpp
+++ Menu.cpp
@@ -1,347 +1,177 @@
 #include "SDL/SDL_ttf.h"
 #include "SDL/SDL.h"
 #include "SDL/SDL_image.h"
-#include "sdlFunctions.h"
-#include "Game.h"
-#include "searchDirectory.h"
+#include "EditorFunctions.h"
 #include <string>
-#include "printText.h"
-#include <iostream>
-#include "Editor.h"
 #include "StringInput.h"
-#include <fstream>
+#include "sdlFunctions.h"
+#include "printText.h"
+#include "functions.h"
+#include "player.h"
+#include "character.h"
+#include "Editor.h"
+#include <iostream>
+
 
 using namespace std;
 
-int menu(SDL_Surface *screen,SDL_Event event);
-int playChoiceMenu(SDL_Surface *screen, SDL_Event event);
-int editChoiceMenu(SDL_Surface *screen, SDL_Event event);
-int editAdventureMenu(SDL_Surface *screen, SDL_Event event);
-int playAdventureMenu(SDL_Surface *screen, SDL_Event event, string filePath);
-int editNewAdventureFile(SDL_Surface *screen, SDL_Event event);
-int editNewAdventureImage(SDL_Surface *screen, SDL_Event event, string text);
+//Editor lägets funktion
+int editor(SDL_Surface *screen, SDL_Event event, string fileName, string imagePath){
 
-//Vår main funktion som initierar skärmen och sdl biblioteken och kallar på menu
-int main(int argc, char* argv[]){
-  SDL_Surface *screen = NULL;
-  const int width = 1024;
-  const int height = 720;
-  const int bpp = 32;
-  SDL_Event event;
-  SDL_Init(SDL_INIT_EVERYTHING);
-  TTF_Init();
-  SDL_WM_SetCaption("Adventure", NULL);
-  screen = SDL_SetVideoMode(width,height,bpp,SDL_SWSURFACE);
-  menu(screen,event);
-}
-
-//Funktionen för huvudmenyn
-int menu(SDL_Surface *screen, SDL_Event event){
-  SDL_Surface *menuImage = NULL;
-  bool quit = false;
-  int mx = 0;
-  int my = 0;
-  menuImage = IMG_Load("mainMenu.png");
-
-  //Hanteringen av alla musklick så länge man är i huvudmenyn
-  while(quit == false){
-    applySurface(0,0, menuImage, screen);
-    while(SDL_PollEvent(&event)){
-      if(event.type == SDL_MOUSEBUTTONDOWN){
-	if( event.button.button == SDL_BUTTON_LEFT ){
-	  mx = event.button.x;                              //mus kordinater
-	  my = event.button.y;
-	  if((mx > 275) && (mx < 718) && (my > 180) && (my < 273)){   //vid tryck på knappen play kallas nästa funktion
-	    int returnStatus = playChoiceMenu(screen,event);
-	    if(returnStatus == 5)                               //när man får en femma betyder det att man har tryckt på krysset i någon 
-	      return 0;                                         //tidigare funktion och proggramet avslutas
-	  }
-	  else if((mx > 275) && (mx < 718) && (my > 305) && (my < 398)){   //vid tryck på knappen edit kallas nästa funktion
-	    int returnStatus = editChoiceMenu(screen,event);
-	    if(returnStatus == 5)
-	      return 0;
-	  }
-	  else if((mx > 275) && (mx < 718) && (my > 430) && (my < 523)){   //vid tryck på knappen quit kavslutas programmet
-	    quit = true;
-	  }
-	}
-      }
-      if(event.type == SDL_QUIT){    // Vid tryck på krysset avslutas programmet
-        quit = true;
-      }
-    }
-    SDL_Flip(screen); // uppdaterar skärmen
-
-  }
-
-  return 0;
-}
-
-//Funktion för den första edit menyn
-int editChoiceMenu(SDL_Surface *screen, SDL_Event event){
-  SDL_Surface *menuImage = NULL;
-  bool quit = false;
-  int mx = 0;
-  int my = 0;
-  menuImage = IMG_Load("editChoiceMenu.png");
-   //Hantering av musklick sålänge man är i menyn
-    while(quit == false){
-    applySurface(0,0, menuImage, screen);
-    while(SDL_PollEvent(&event)){
-      if(event.type == SDL_MOUSEBUTTONDOWN){
-	if( event.button.button == SDL_BUTTON_LEFT ){
-	  mx = event.button.x;
-	  my = event.button.y;
-	  if((mx > 275) && (mx < 718) && (my > 180) && (my < 273)){  //vid tryck på knappen create new adventure kallas nästa funktion
-	    int returnStatus = editNewAdventureFile(screen,event);
-	    if(returnStatus == 5)                                  //här returnar man en femma till funktionen ovanför
-	      return 5;                                            // för att berätta att krysset har tryckts
-	  }
-	  else if((mx > 275) && (mx < 718) && (my > 305) && (my < 398)){ //vid tryck på knappen continue edit kallas nästa funktion
-	    int returnStatus = editAdventureMenu(screen,event);
-	    if(returnStatus == 5)
-	      return 5;
-	  }
-	  else if((mx > 275) && (mx < 718) && (my > 430) && (my < 523)){  //vid tryck på knappen back går man tillbaka till föregående meny
-	    return 0;
-	  }
-	}
-      }
-      if(event.type == SDL_QUIT){  //krysset returnar en femma till funktionen ovanför
-	  return 5;
-      }
-    }
-    SDL_Flip(screen);
-
-  }
-  return 0;
-}
-
-//Funktionen för den första play undermenyn
-int playChoiceMenu(SDL_Surface *screen, SDL_Event event){
-  SDL_Surface *menuImage = NULL;
-  bool quit = false;
-  int mx = 0;
-  int my = 0;
-  menuImage = IMG_Load("playChoiceMenu.png");
-  //Hantering av musklick så länge man är i menyn
-  while(quit == false){
-    applySurface(0,0, menuImage, screen);
-    while(SDL_PollEvent(&event)){
-      if(event.type == SDL_MOUSEBUTTONDOWN){
-	if( event.button.button == SDL_BUTTON_LEFT ){
-	  mx = event.button.x;
-	  my = event.button.y;
-	  if((mx > 275) && (mx < 718) && (my > 180) && (my < 273)){  //vid tryck på knappen new adventure kallas nästa funktion
-	    int returnStatus = playAdventureMenu(screen,event, "Adventures");	    
-	    if(returnStatus == 5)
-	      return 5;
-	  }
-	  else if((mx > 275) && (mx < 718) && (my > 305) && (my < 398)){  //vid tryck på knappen continue adventure kallas nästa funktion
-	    int returnStatus = playAdventureMenu(screen,event, "Saved games");
-	    if(returnStatus == 5)
-	      return 5;
-	  }
-	  else if((mx > 275) && (mx < 718) && (my > 430) && (my < 523)){  //vid tryck på knappen back går man tillbaka till föregående meny
-	    return 0;
-	  }
-	}
-      }
-      if(event.type == SDL_QUIT){
-	return 5;
-      }
-    }
-    SDL_Flip(screen);
-    
-  }
-  return 0;
-}
-
-//Funktionen för continue adventure undermenyn
-int playAdventureMenu(SDL_Surface *screen, SDL_Event event, string filePath){
-  SDL_Surface *menuImage = NULL;
+  SDL_Surface *background = NULL;
   SDL_Surface *message = NULL;
-  bool quit = false;
-  string temp;
-  int pos = 0;
-  string fileNames = searchD(filePath); //hämtar alla textfilers namn som ligger i rätt mapp
+  SDL_Surface *roomImage = NULL;
   int mx = 0;
-  int my = 0;
-  TTF_Font *font = NULL;
+  int my = 0;                                                     //mus kordinater
   SDL_Color textColor = {255, 255, 255}; 
-  font = TTF_OpenFont("arial.ttf", 28);
-  menuImage = IMG_Load("playChooseAdventure.png");
-  //Hantering av musklick så länge man är i menyn
-  while(quit == false){
-    applySurface(0,0, menuImage, screen);
-    printText(fileNames,292 , 146,textColor,message,font,screen);
-    while(SDL_PollEvent(&event)){
-      if(event.type == SDL_MOUSEBUTTONDOWN){
-        if( event.button.button == SDL_BUTTON_LEFT ){
-	  mx = event.button.x;
-	  my = event.button.y;
-	  for(int i = 0; i < fileNames.length()/36; ++i){   //beroende på hur många filnamn, bestämmer var på skärmen man kan klicka för att
-	    if((mx > 282) && (mx < 736) && (my > (146+28*i+3)) && (my < (146+28*(i+1)+3))){  // aktivera de olika filerna
-	      pos = fileNames.find(" ", 36*i);
-	      temp = fileNames.substr(36*i, pos-36*i);
-	      int returnStatus = game(screen,event,filePath + "//" + temp);
-	      if(returnStatus == 5)
-		return 5;
-	    }
-	    else if((mx > 278) && (mx < 741) && (my > 603) && (my < 681) ){ //vid tryck på knappen back går man tillbaka till föregående meny
-	      quit = true;
-	    }
-	  }
-        }
-      }
-      if(event.type == SDL_QUIT){
-        return 5;
-      }
-    }
-    SDL_Flip(screen);
-    
-  }
-  return 0;
-}
-
-//Funktion för continue edit undermeny
-int editAdventureMenu(SDL_Surface *screen, SDL_Event event){
-  SDL_Surface *menuImage = NULL;
-  SDL_Surface *message = NULL;
-  bool quit = false;
-  string temp;
-  int pos = 0;
-  string fileNames = searchD("Adventures");  //hämtar namn från mappen Adventures
-  int mx = 0;
-  int my = 0;
   TTF_Font *font = NULL;
-  SDL_Color textColor = {255, 255, 255};
-  font = TTF_OpenFont("arial.ttf", 28);
-  menuImage = IMG_Load("editorChooseAdventure.png");
-  //Hantering av musklick så länge man är i menyn
-  while(quit == false){
-    applySurface(0,0, menuImage, screen);
-    printText(fileNames,292 , 146,textColor,message,font,screen);
-    while(SDL_PollEvent(&event)){
-      if(event.type == SDL_MOUSEBUTTONDOWN){
-	if( event.button.button == SDL_BUTTON_LEFT ){
-	  mx = event.button.x;
-	  my = event.button.y;
-	  for(int i = 0; i < fileNames.length()/36; ++i){  //beroende på hur många filnamn, bestämmer var på skärmen man kan klicka för att
-	    if((mx > 282) && (mx < 736) && (my > (146+28*i+3)) && (my < (146+28*(i+1)+3))){  // aktivera de olika filerna
-	      pos = fileNames.find(" ", 36*i);
-	      temp = fileNames.substr(36*i, pos-36*i);
-	      int returnStatus = editor(screen,event,temp,"");
-	      if(returnStatus == 5)
-		return 5;
-	    }
-	    else if((mx > 278) && (mx < 741) && (my > 603) && (my < 681) ){ //vid tryck på knappen back går man tillbaka till föregående meny
-	      quit = true;
-	    }
-	  }
-	}
-      }
-      if(event.type == SDL_QUIT){
-	return 5;
-      }
-    }
-    SDL_Flip(screen);  
-  }
-  return 0;
-}
-
-//Funktion för create new adventure undermeny
-int editNewAdventureFile(SDL_Surface *screen, SDL_Event event){
-  SDL_Surface *menuImage = NULL;
   bool quit = false;
-  int textLength = 20;
-  int mx = 0;
-  int my = 0;
+  background = IMG_Load("editorBackgroundImage.png");
+  font = TTF_OpenFont("arial.ttf", 28);
+  int ifStatementToUse = 0;
+  int textLength = 330;
   StringInput inputText;
   string text;
-  menuImage = IMG_Load("newAdventureFilename.png");
-  //Hantering av mus och tangent tryck så länge man är i menyn
+  string text3 = "";
+  string whatToType = "";                           //Initiering av variabler
+
+  //Editorn lägets while loop, sålänge man är i while loopen så är man i editorn
   while(quit == false){
-    applySurface(0,0, menuImage, screen);
-    while(SDL_PollEvent(&event)){
-      inputText.handleInput(event,textLength);      
-      if((event.type == SDL_KEYDOWN) && (event.key.keysym.sym == SDLK_RETURN)){
-	text = inputText.getStr();     //lägger in den inskrivda texten i strängen text
-	ofstream newFile;
-	newFile.open("Adventures//" + text + ".txt");     //Skapar den nya filen i mappen Adventures
-	if(!newFile.is_open())
-	  cerr << "couldn't create: " << text << "\n"; 
-	newFile.close();
-	int returnStatus = editNewAdventureImage(screen,event,text);
-	if(returnStatus == 1)    // om man får tillbaka en etta ska man gå tillbaka till föregående menyn 
-	  return 0;
-	else if(returnStatus == 5)
-	  return 5;
-	inputText.clearString();
-      }
-      if(event.type == SDL_MOUSEBUTTONDOWN){
-	if( event.button.button == SDL_BUTTON_LEFT ){
-	  mx = event.button.x;
-	  my = event.button.y;
-	  
-	  if((mx > 275) && (mx < 718) && (my > 430) && (my < 523)){ //vid tryck på knappen back går man tillbaka till föregående meny
-	    return 0;
-	  }
-	}
-      }
-      if(event.type == SDL_QUIT){
-	  return 5;
-      }
-    }
-
-    inputText.showMessage(screen, 400, 350); //visar den inskrivna texten på skärmen
-    SDL_Flip(screen);
-
-  }
-  return 0;
-}
-
-//Funktion för standardbilden som man kommer till efter create new adventure undermeny
-int editNewAdventureImage(SDL_Surface *screen, SDL_Event event, string text){
-  SDL_Surface *menuImage = NULL;
-  bool quit = false;
-  int textLength = 20;
-  int mx = 0;
-  int my = 0;
-  StringInput inputText;
-  string imagePath;
-  menuImage = IMG_Load("defaultImageName.png");
-  //Hantering av mus och tangent tryck så länge man är i menyn
-  while(quit == false){
-    applySurface(0,0, menuImage, screen);
+    applySurface(0,0, background, screen);
+    printText(whatToType,10 , 10,textColor,message,font,screen);                                //Skriver ut text
+    printText(text, 10, (whatToType.length()/36)*28 + 50,textColor,message,font,screen);        //skriver ut dynamsik text som skiftar i y led beroende på hur lång whatToType är
+    //Hantering av mus och tangent tryck i editorn
     while(SDL_PollEvent(&event)){
       inputText.handleInput(event,textLength);
+
       if((event.type == SDL_KEYDOWN) && (event.key.keysym.sym == SDLK_RETURN)){
-	imagePath = inputText.getStr();              //lägger in det inskrivna bildnamnet i strängen imagePath
-	int returnStatus = editor(screen,event,text,"Adventures//" + imagePath + ".png"); 
-	if(returnStatus == 1)
-	  return 1;
-	else if(returnStatus == 5)
-	  return 5;
+
+        string cmd = inputText.getStr();                                                         //Lagra inputten från tangentbordet i strängen cmd
+
+	//if satser för alla olika kommandon i editorn
+	//alla kommandona har två lägen (if satser) först så letar vi efter en sträng, när denna sträng hittas så skrivs en hjälptext ut och
+	// ifStatementToUse sätts till till ett unikt värde som sen triggar igång en till if sats där man får skriva in det som faktiskt kommer hamna i text filen
+        if(cmd == "Makeroom" || cmd == "makeroom"){
+         whatToType = "Type Id;Name;Description;ImagePath; DoorId;DoorId;DoorId;DoorId;winRoom; Press help for further explanations";
+         cout<<"Fil namn:"<< fileName<< endl;
+         ifStatementToUse = 1;
+        }
+        else if(ifStatementToUse == 1){
+         if(roomToFile(cmd, imagePath, fileName) == false)
+         whatToType = "Wrong input format";
+         else
+         whatToType = "Creation successful";
+         ifStatementToUse = 0;
+        }
+
+        else if(cmd == "Makeitem" || cmd == "makeitem") {
+         whatToType = "Type roomId;item id;Name;Text;";
+         ifStatementToUse = 2;
+        }
+        else if(ifStatementToUse == 2){
+         if(itemToFile(cmd, fileName) == false)
+         whatToType = "Wrong input format";
+         else
+         whatToType = "Creation successful";
+         ifStatementToUse = 0;
+        }
+        else if(cmd == "Makecharacter" || cmd == "makecharacter") {
+         whatToType = "Type roomId;character id;Name;Text;";
+         ifStatementToUse = 3;
+        }
+        else if(ifStatementToUse == 3){
+         if(characterToFile(cmd, fileName) == false)
+         whatToType = "Wrong input format";
+         else
+         whatToType = "Creation successful";
+         ifStatementToUse = 0;
+        }
+        else if(cmd == "Deleteroom" || cmd == "deleteroom"){
+         whatToType = "Type roomId";
+         ifStatementToUse = 4;
+        }
+        else if(ifStatementToUse == 4){
+         if(deleteRoom(cmd, fileName) == false)
+         whatToType = "Could not delete room";
+         else
+         whatToType = "Deletion successful";
+         ifStatementToUse = 0;
+        }
+        else if(cmd == "Deleteitem" || cmd == "deleteitem"){
+         whatToType = "Type itemId";
+         ifStatementToUse = 5;
+        }
+        else if(ifStatementToUse == 5){
+         if(deleteItem(cmd, fileName) == false)
+         whatToType = "Could not delete item";
+         else
+         whatToType = "Deletetion successful";
+         ifStatementToUse = 0;
+        }
+        else if(cmd == "Deletcharacter" || cmd == "deletecharacter"){
+         whatToType = "Type characterId";
+         ifStatementToUse = 6;
+        }
+        else if(ifStatementToUse == 6){
+         if(deleteCharacter(cmd, fileName) == false)
+         whatToType = "Could not delete character";
+         else
+         whatToType = "Deletion successful";
+         ifStatementToUse = 0;
+        }
+
+        else{                    //om kommandot inte finns skrivs unknown command ut
+         text = "Unknown command";
+        }
+        inputText.clearString();
+        SDL_FreeSurface(message);
+
+      }
+      if(event.type == SDL_QUIT){
+	setCurrentRoom(fileName);
+	return 5;
       }
       if(event.type == SDL_MOUSEBUTTONDOWN){
 	if( event.button.button == SDL_BUTTON_LEFT ){
 	  mx = event.button.x;
 	  my = event.button.y;
-	  
-	  if((mx > 275) && (mx < 718) && (my > 430) && (my < 523)){ //vid tryck på back knappen går tillbaka till den första edit undermenyn
-	    return 1;
+	  if((mx > 610) && (mx < 935) && (my > 349) && (my < 435)){   //vid tryck på sav and quit så går man tillbaka till den föregående menyn allternativ den första 
+	    setCurrentRoom(fileName);                                 //editor undermenyn ifall det är ett ny skapat äventyr 
+	    return 1;                                                 //när man avslutar så sätts även ett current room annars kan saker gå sönder när man försöker starta spelet
+	  }
+	  else if((mx > 610) && (mx < 935) && (my > 203) && (my < 289)){   //vid tryck på help knappen så skrivs hjälp texten ut
+	    text = helpFunction(ifStatementToUse);
 	  }
 	}
       }
-      if(event.type == SDL_QUIT){
-	  return 5;
-      }
     }
+    inputText.showMessage(screen, 10,675);   //skriver ut inputt från tangentbordet på skärmen
 
-    inputText.showMessage(screen, 400, 350);
-    SDL_Flip(screen);
+    SDL_Flip(screen);                        //uppdaterar skärmen
+
 
   }
-  return 0;
+
+  cleanUp(background,message,font);//städar upp vid avslut
+
+}
+
+//Funktion för att skriva ut hjälp text i editorn
+string helpFunction(const int helpStatus){
+  //skriver ut olika strängar beroende på vilket steg i editorn man är i
+  string returnString;
+  if(helpStatus == 1){
+    returnString += "roomId=1XXX, ImagePath=imagename.png, DoorId=<id to a room>1XXX, winRoom=(1 or 0)";
+  }
+  else if(helpStatus == 2){
+    returnString += "roomId=1XXX, itemId=2XXX";
+  }
+  else if(helpStatus == 3){
+    returnString += "roomId=1XXX, characterId=3XXX";
+  }
+  else{
+    returnString += "Commands available: " ;
+    returnString += "'Makeroom', 'Makeitem', 'Makecharacter', 'Deleteroom', 'Deleteitem', 'Deletecharacter'";   //om man inte har skrivit in något alls så skrivs de olika kommandona ut som man kan skriva
+  }
+  return returnString;
 }
