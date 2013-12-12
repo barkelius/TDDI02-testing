dbee637b2595d187391577638e1853a011df5546
arvjo <riavd18@gmail.com>
2013-12-12 12:43:44 +0100

    Update printText.cpp

--- printText.cpp
+++ printText.cpp
@@ -8,23 +8,24 @@
 
 using namespace std;
 
+//Skriver ut all text som man hämtar från text filen
 void printText(string text, int x, int y, SDL_Color textColor,SDL_Surface* message,TTF_Font* font,SDL_Surface* screen){
-  
+
   vector<string> lines;
   string temp;
   while(text.length() > 37){
     int pos = text.rfind(" ",37);
-    temp = text.substr(pos+1,text.length());
+    temp = text.substr(pos+1,text.length());     //delar upp texten i flera rader 
     text.resize(pos);
     lines.push_back(text);;
     text = temp;
   }
-  
+
   lines.push_back(text);
-  
+
   for(int i = 0; i < lines.size(); ++i){
     message = TTF_RenderText_Solid(font,lines[i].c_str(),textColor);
-    applySurface(x,(y+(i *20)), message, screen);
+    applySurface(x,(y+(i *28)), message, screen);       //skriver ut alla raderna som är lagrade i vektorn lines
   }
- 
+
 }
